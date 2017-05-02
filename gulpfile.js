890551b902c81fb80521550a82fe7fbf634d8d4a
Sanjeev Prajapati <spr@codebee.dk>
2017-05-02 16:53:33 +0545

    initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var gulpless = require('gulp-less');
+var gulpkss = require('gulp-kss');
+var gulpconcat = require('gulp-concat');
+
+
+gulp.task('kss', function () {
+ // Generate styleguide with templates 
+ gulp.src(['styles/**/*.less'])
+ .pipe(gulpkss({
+ 	overview: __dirname + '/styles/styleguide.md'
+ }))
+ .pipe(gulp.dest('styleguide/'));
+
+// Concat and compile all your styles for correct rendering of the styleguide. 
+gulp.src('styles/main.less')
+.pipe(gulpless())
+.pipe(gulpconcat('public/style.css'))
+.pipe(gulp.dest('styleguide/'));
+});
+
+
+
+gulp.task('default', ['kss']);
